0aa13d53253dc280d26ce83d081a4a9a871b46ad
SenepartM <113522450+SenepartM@users.noreply.github.com>
2023-12-21 14:48:11 +0100

    suppression footer ( pub )

--- modification.php
+++ modification.php
@@ -193,13 +193,9 @@ $_SESSION['user_id'] = $row['id']; ?>
 
   </section>
   
-  <footer class="page-footer">
-    <span>made by </span>
-    <a href="https://georgemartsoukos.com/" target="_blank">
-      <img width="24" height="24" src="https://assets.codepen.io/162656/george-martsoukos-small-logo.svg" alt="George Martsoukos logo">
-    </a>
+  
     <script  src="./scriptt.js"></script>
-  </footer>
+  
 </section>
 
 
